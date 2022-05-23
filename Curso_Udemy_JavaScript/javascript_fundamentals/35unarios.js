09d05b497788b6582cfb1faa75b09f5de674f7a2
williammmoura <williamonte@gmail.com>
2022-05-23 17:03:32 -0300

    js

---
+++ Curso_Udemy_JavaScript/javascript_fundamentals/35unarios.js
@@ -0,0 +1,10 @@
+let num1 = 1
+let num2 = 2
+
+num1++;
+console.log(num1);
+--num1; //prioridade maior (ordem de precedência).
+console.log(num1);
+
+console.log(++num1 === num2--); //Difícil leitura do código.
+console.log(num1 === num2); //Código mais fácil de ser lido.
---
+++ Curso_Udemy_JavaScript/javascript_fundamentals/37experimentos.js;.js
@@ -0,0 +1,21 @@
+/**Exemplo de Node */
+//Aula para conhecer o local onde está executando (RunTime).
+
+let a = 3;
+
+global.b = 123;
+this.c = 456;
+this.e = 'teste';
+
+console.log(a);
+console.log(global.b);
+console.log(this.c);
+console.log(module.exports.c);
+console.log(module.exports === this);
+console.log(module.exports);
+
+//Criando uma variável maluca, ou seja, sem var e let.
+abc = 3; //não fazer isso!
+console.log(global.abc);
+
+//module.exports = { e: 456, f: false, g: 'teste' }
