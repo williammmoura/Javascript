fc9701b895ad103a3fe2f2e737fa0aa657284882
William Monte de Moura <williamonte@gmail.com>
2022-06-06 15:57:16 -0300

    js

---
+++ Curso_Udemy_JavaScript/06Funcao/03paramsVariaveis.js
@@ -0,0 +1,14 @@
+function soma(){
+    let soma = 0
+    for (i in arguments) {
+        soma += arguments[i] //cada índice do array vira um argumento. 
+    }
+    return soma
+}
+
+//Chamando a função:
+console.log(soma())  //Repare, que não foi passado nenhum parâmetro a variável soma.
+console.log(soma(1))
+console.log(soma(1.1, 2.2, 3.3)) //Será feita a soma dos argumentos.
+console.log(soma(1.1, 2.2, 'Teste')) //Aqui, haverá a soma dos numeros e a concatenação da string "Teste".
+console.log(soma('William', 'Carol', 'Maggie')) //Concatenação das strings. O zero, já estará na variável soma.
