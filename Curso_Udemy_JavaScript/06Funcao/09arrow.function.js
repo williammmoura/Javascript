c472ef320dcfbf238912f5ad8641a0ab62981530
William Monte de Moura <williamonte@gmail.com>
2022-06-08 18:27:20 -0300

    js

---
+++ Curso_Udemy_JavaScript/06Funcao/09arrow.function.js
@@ -0,0 +1,19 @@
+let comparaComThis = function (param) {
+    console.log(this === param) //Escopo Global
+}
+
+comparaComThis(global)
+
+const obj = {}
+comparaComThis = comparaComThis.bind(obj)
+comparaComThis(global)
+comparaComThis(obj)
+
+let comparaComThisArrow = param => console.log(this === param)
+comparaComThisArrow(global)
+comparaComThisArrow(module.exports)
+comparaComThisArrow(this)
+
+comparaComThisArrow = comparaComThisArrow.bind(obj)
+comparaComThisArrow(obj)
+comparaComThisArrow(module.exports)
