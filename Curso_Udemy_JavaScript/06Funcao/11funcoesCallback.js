0be6dab2c54429ba65ece8729dea6a2c9eed3a85
William Monte de Moura <williamonte@gmail.com>
2022-06-13 15:35:55 -0300

    js

---
+++ Curso_Udemy_JavaScript/06Funcao/11funcoesCallback.js
@@ -0,0 +1,13 @@
+/**Callback: passar uma função para outra função e quando determinado evento acontecer,
+ * a função, que foi passada, vai ser disparada de volta. A função "callback" pode ser
+ * chamada várias vezes, ou apenas uma única vez (depende do contexto). 
+*/
+
+const fabricantes = ["Mercedes", "Audi", "BMW"]
+
+function imprimir(nome, indice){
+    console.log(`${indice + 1}, ${nome}`)
+}
+
+fabricantes.forEach(imprimir)/**"forEach" é uma função de fabricantes(array) */
+fabricantes.forEach(fabricante => console.log(fabricante))
---
+++ Curso_Udemy_JavaScript/06Funcao/12funcoesCallback.js
@@ -0,0 +1,21 @@
+const notas = [7.7, 6.5, 5.2, 8.9, 3.6, 7.1, 9.0]
+
+//Sem Callback
+const notasBaixas1 = []
+for(let i in notas){
+    if(notas[i] < 7)/**teste "if"*/{
+        notasBaixas1.push(notas[i]) //".push" adiciona elementos no array.
+    }
+}
+
+console.log(notasBaixas1)
+
+//Com CAllback
+notasBaixas2 = notas.filter(function(nota){
+    return nota < 7
+})
+
+console.log(notasBaixas2)
+
+const notasBaixas3 = notas.filter(nota => nota < 7)
+console.log(notasBaixas3)
