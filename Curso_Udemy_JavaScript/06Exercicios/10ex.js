dd989c622e16180a17435a1040987546d4ecc8b3
William Monte de Moura <williamonte@gmail.com>
2022-06-23 21:27:50 -0300

    ex-js

--- Curso_Udemy_JavaScript/06Exercicios/10ex.js
+++ Curso_Udemy_JavaScript/06Exercicios/10ex.js
@@ -1,2 +1,13 @@
 /**Crie uma função que verifica se um número inteiro passado como parêmetro é divisível por 3 e retorne true
 ou false.*/
+
+function divisivel3(x){
+    if(x % 3 == 0){
+        return true
+    }else{
+        return false
+    }
+}
+
+console.log(divisivel3(3))
+console.log(divisivel3(8))
