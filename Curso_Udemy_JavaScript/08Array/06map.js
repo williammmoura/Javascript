8247ddd688e43b5da0c548b5dbc352b4e89f0f14
William Monte de Moura <williamonte@gmail.com>
2022-08-25 17:13:19 -0300

    map.js

--- Curso_Udemy_JavaScript/08Array/06map.js
+++ Curso_Udemy_JavaScript/08Array/06map.js
@@ -21,4 +21,4 @@ const triplo = e => e * 3;
 const paraDinheiro = e => `R$ ${parseFloat(e).toFixed(2).replace('.',',')}`
 
 resultado = nums.map(soma10).map(triplo).map(paraDinheiro);
-console.log(resultado);//Resultado aparece e uma "string" pois está em uma template string.
+console.log(resultado);//Resultado aparece em uma "string" pois está em uma template string.
