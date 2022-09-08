b1966c93af0d2c07df65876d82de11f346197c10
William Monte de Moura <williamonte@gmail.com>
2022-09-08 19:31:24 -0300

    js

--- Curso_Udemy_JavaScript/08Array/10filter.js
+++ Curso_Udemy_JavaScript/08Array/10filter.js
@@ -1,11 +1,10 @@
 Array.prototype.filter2 = function(callback){
-    const newArray = [
-        for(let i = 0; i < this.length; i++){
-            if(callback(this[i], i, this)){
-                newArray.push(this[i])
-            }
+    const newArray = []
+    for(let i = 0; i < this.length; i++){
+        if(callback(this[i], i, this)){
+            newArray.push(this[i])
         }
-    ]
+    }
 }
 
 const produtos = [
