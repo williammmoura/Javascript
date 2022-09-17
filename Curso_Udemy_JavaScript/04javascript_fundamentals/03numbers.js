d1beb746cd9e4d5885dbb532439a0e9da90aec8f
William Monte de Moura <williamonte@gmail.com>
2022-09-17 20:02:58 -0300

    js

--- Curso_Udemy_JavaScript/04javascript_fundamentals/03numbers.js
+++ Curso_Udemy_JavaScript/04javascript_fundamentals/03numbers.js
@@ -2,8 +2,8 @@ const peso1 = 1.0;
 const peso2 = Number('2.0');
 
 console.log(peso1, peso2);
-console.log(Number.isInteger(peso1));
-console.log(Number.isInteger(peso2));
+console.log(Number.isInteger(peso1));//Verificando se o número é inteiro (true).
+console.log(Number.isInteger(peso2));//Verificando se o número é inteiro (true).
 
 const avaliacao1 = 9.871;
 const avaliacao2 = 6.871;
