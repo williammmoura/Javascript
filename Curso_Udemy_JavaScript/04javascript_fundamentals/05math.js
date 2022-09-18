b233f7c077e64a43838a379d5e92b7dce5ccc9a4
William Monte de Moura <williamonte@gmail.com>
2022-09-18 18:22:27 -0300

    js

--- Curso_Udemy_JavaScript/04javascript_fundamentals/05math.js
+++ Curso_Udemy_JavaScript/04javascript_fundamentals/05math.js
@@ -1,5 +1,5 @@
 //Objeto Math
-/*Math*/
+/*Math é um objeto embutido que tem propriedades e métodos para constantes e funções matemáticas. Não é um objeto de função.*/
 const raio = 5.6;
 const area = Math.PI * Math.pow(raio,2);/*Math.pow
 fará a exponenciação.*/
