26f9bcd2948814bfd8b1c3d01b7a17f3b747abc1
William Monte de Moura <williamonte@gmail.com>
2022-09-18 18:20:02 -0300

    js

--- Curso_Udemy_JavaScript/04javascript_fundamentals/05math.js
+++ Curso_Udemy_JavaScript/04javascript_fundamentals/05math.js
@@ -1,6 +1,8 @@
+//Objeto Math
+/*Math*/
 const raio = 5.6;
 const area = Math.PI * Math.pow(raio,2);/*Math.pow
 fará a exponenciação.*/
 
 console.log(area.toFixed(2));
-console.log(typeof Math);
+console.log(typeof Math);//Objeto.
