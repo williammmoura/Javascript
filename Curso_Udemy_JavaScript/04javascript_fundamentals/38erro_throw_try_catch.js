bc64c4780aae7437d86bf3f129e5dc3f541136ff
William Monte de Moura <williamonte@gmail.com>
2022-09-20 17:38:51 -0300

    js

--- Curso_Udemy_JavaScript/04javascript_fundamentals/38erro_throw_try_catch.js
+++ Curso_Udemy_JavaScript/04javascript_fundamentals/38erro_throw_try_catch.js
@@ -1,7 +1,7 @@
 function tratarErroELancar(erro) {
     //throw new Error('...');
     //throw 10
-    throw {
+    throw {//Lançamento do erro. No caso foi lançado um objeto.
         nome: erro.name,
         msg: erro.message,
         date: new Date
@@ -9,11 +9,11 @@ function tratarErroELancar(erro) {
 }
 
 function imprimirNomeGritado(obj) {
-    try{
+    try{//try -> Bloco de código que pode gerar um tipo de erro.
         console.log(obj.name.toUpperCase() + '!!!')
-    } catch (e) {
+    } catch (e) {//catch -> Tratamento do erro, que foi geredo dentro do "try".
         tratarErroELancar(e)
-    } finally {
+    } finally {//Será executado mesmo que ocorra um erro ou não. 
         console.log('final')
     }
 }
